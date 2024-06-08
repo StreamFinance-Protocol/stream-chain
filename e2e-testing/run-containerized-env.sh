c8f99d7e3f87ad28daf9a9c98da122410fbf5776
EC2 Default User <ec2-user@ip-172-31-31-127.ec2.internal>
2024-06-08 03:56:08 +0000

    conflict

--- e2e-testing/run-containerized-env.sh
+++ e2e-testing/run-containerized-env.sh
@@ -1,7 +1,10 @@
 cd ../protocol
 make e2etest-build-image
 cd ../e2e-testing
+<<<<<<< HEAD
 
 #when building w/o  a cache you must split the command into two
+=======
+>>>>>>> 552bd4378 (e2e test)
 docker-compose -f docker-compose-e2e-test.yml build --no-cache
 docker-compose -f docker-compose-e2e-test.yml up

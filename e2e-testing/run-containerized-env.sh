344d3059f3c19f03d754687490999641a1ee198c
EC2 Default User <ec2-user@ip-172-31-31-127.ec2.internal>
2024-06-08 03:56:14 +0000

    conflict

--- e2e-testing/run-containerized-env.sh
+++ e2e-testing/run-containerized-env.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 cd ../protocol
 make build-e2e-ethos-test
 cd ../e2e-testing
@@ -6,5 +7,11 @@ cd ../e2e-testing
 #when building w/o  a cache you must split the command into two
 =======
 >>>>>>> 552bd4378 (e2e test)
+=======
+
+
+>>>>>>> 613d96696 (checkpoint)
 docker-compose -f docker-compose-e2e-test.yml build --no-cache
-docker-compose -f docker-compose-e2e-test.yml up
+docker-compose -f docker-compose-e2e-test.yml up -d
+cd ../protocol
+make e2e-setup

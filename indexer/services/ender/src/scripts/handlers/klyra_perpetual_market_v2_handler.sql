947579c40b82d2d920dff1710aeb46731fd71a3d
0xDro <alegala21@gmail.com>
2024-12-19 15:03:31 -0800

    [MULTI-27] Indexer: handle on chain events of creating perpetuals with new collateral pool id param in ender

--- indexer/services/ender/src/scripts/handlers/klyra_perpetual_market_v2_handler.sql
+++ indexer/services/ender/src/scripts/handlers/klyra_perpetual_market_v2_handler.sql
@@ -24,6 +24,7 @@ BEGIN
     perpetual_market_record."quantumConversionExponent" = (event_data->'quantumConversionExponent')::integer;
     perpetual_market_record."atomicResolution" = (event_data->'atomicResolution')::integer;
     perpetual_market_record."dangerIndexPpm" = (event_data->'dangerIndexPpm')::integer;
+    perpetual_market_record."collateralPoolId" = (event_data->'collateralPoolId')::integer;
     perpetual_market_record."subticksPerTick" = (event_data->'subticksPerTick')::integer;
     perpetual_market_record."stepBaseQuantums" = klyra_from_jsonlib_long(event_data->'stepBaseQuantums');
     perpetual_market_record."liquidityTierId" = (event_data->'liquidityTier')::integer;

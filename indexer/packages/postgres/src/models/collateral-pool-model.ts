3f5e7cbc764314e783e5c010c1304e7872fe16a4
0xDro <alegala21@gmail.com>
2024-12-22 00:06:56 -0500

    [MULTI-28] Add collateral pool postgres model

---
+++ indexer/packages/postgres/src/models/collateral-pool-model.ts
@@ -0,0 +1,58 @@
+import { Model } from 'objection';
+
+export default class CollateralPoolsModel extends Model {
+  static get tableName() {
+    return 'collateral_pools';
+  }
+
+  static get idColumn() {
+    return 'id';
+  }
+
+  static relationMappings = {};
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: [
+        'id',
+        'maxCumulativeInsuranceFundDeltaPerBlock',
+        'multiCollateralAssets',
+        'quoteAssetId',
+      ],
+      properties: {
+        id: { type: 'integer' },
+        maxCumulativeInsuranceFundDeltaPerBlock: { type: 'integer' },
+        multiCollateralAssets: {
+          type: 'string',
+          items: { type: 'integer', minimum: 0 },
+          postgresql: { type: 'integer[]' },
+        },
+        quoteAssetId: { type: 'integer' },
+      },
+    };
+  }
+
+  /**
+   * A mapping from column name to JSON conversion expected.
+   * See getSqlConversionForKlyraModelTypes for valid conversions.
+   *
+   * TODO(IND-239): Ensure that jsonSchema() / sqlToJsonConversions() / model fields match.
+   */
+  static get sqlToJsonConversions() {
+    return {
+      id: 'integer',
+      maxCumulativeInsuranceFundDeltaPerBlock: 'integer',
+      multiCollateralAssets: 'string',
+      quoteAssetId: 'integer',
+    };
+  }
+
+  id!: number;
+
+  maxCumulativeInsuranceFundDeltaPerBlock!: number;
+
+  multiCollateralAssets!: string;
+
+  quoteAssetId!: number;
+}

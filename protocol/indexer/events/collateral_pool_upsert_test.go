7470224ed94d9c85740f75967658c119fb080fee
0xDro <alegala21@gmail.com>
2024-12-23 08:46:23 -0500

    [MULTI-28] Testing: fix test naming for changing collat pool event from creation to upsert

--- protocol/indexer/events/collateral_pool_upsert_test.go
+++ protocol/indexer/events/collateral_pool_upsert_test.go
@@ -7,17 +7,17 @@ import (
 )
 
 func TestNewCollateralPoolUpsertEvent_Success(t *testing.T) {
-	collateralPoolCreateEvent := NewCollateralPoolUpsertEvent(
+	collateralPoolUpsertEvent := NewCollateralPoolUpsertEvent(
 		0,
 		1000000,
 		[]uint32{0},
 		0,
 	)
-	expectedCollateralPoolCreateEventProto := &CollateralPoolUpsertEvent{
+	expectedCollateralPoolUpsertEventProto := &CollateralPoolUpsertEvent{
 		Id:                                      0,
 		MaxCumulativeInsuranceFundDeltaPerBlock: 1000000,
 		MultiCollateralAssets:                   []uint32{0},
 		QuoteAssetId:                            0,
 	}
-	require.Equal(t, expectedCollateralPoolCreateEventProto, collateralPoolCreateEvent)
+	require.Equal(t, expectedCollateralPoolUpsertEventProto, collateralPoolUpsertEvent)
 }

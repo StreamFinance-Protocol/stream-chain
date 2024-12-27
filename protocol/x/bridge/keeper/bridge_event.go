5a2739e094bb8ff09a12e6d9b7f9ae89e562c1e3
0xDro <alegala21@gmail.com>
2024-12-27 17:11:42 -0500

    [BMOD-15] bridge event info handler

---
+++ protocol/x/bridge/keeper/bridge_event.go
@@ -0,0 +1,13 @@
+package keeper
+
+import (
+	"github.com/StreamFinance-Protocol/stream-chain/protocol/x/bridge/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// `GetBridgeEventFromServer` returns the bridge event with the given id from the server. `found` is false
+// if the event is not found.
+func (k Keeper) GetBridgeEventFromServer(ctx sdk.Context, id uint32) (event types.BridgeEvent, found bool) {
+	event, _, found = k.bridgeEventManager.GetBridgeEventById(id)
+	return event, found
+}

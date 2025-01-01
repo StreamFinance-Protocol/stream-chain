d1bcafc66495bbc914c3d5d3e0a1569142ab0440
0xDro <alegala21@gmail.com>
2025-01-01 16:14:35 -0500

    [BMOD-33] add bridge msgs to app injected msgs

--- protocol/app/msgs/app_injected_msgs.go
+++ protocol/app/msgs/app_injected_msgs.go
@@ -1,8 +1,11 @@
 package msgs
 
 import (
+	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	"github.com/StreamFinance-Protocol/stream-chain/protocol/testutil/constants"
+	bridgetypes "github.com/StreamFinance-Protocol/stream-chain/protocol/x/bridge/types"
 	clobtypes "github.com/StreamFinance-Protocol/stream-chain/protocol/x/clob/types"
 	perptypes "github.com/StreamFinance-Protocol/stream-chain/protocol/x/perpetuals/types"
 )
@@ -11,6 +14,19 @@ var (
 	// AppInjectedMsgSamples are msgs that are injected into the block by the proposing validator.
 	// These messages are reserved for proposing validator's use only.
 	AppInjectedMsgSamples = map[string]sdk.Msg{
+		"/dydxprotocol.bridge.MsgAcknowledgeBridges": &bridgetypes.MsgAcknowledgeBridges{
+			Events: []bridgetypes.BridgeEvent{
+				{
+					Id: 0,
+					Coin: sdk.NewCoin(
+						"bridge-token",
+						sdkmath.NewIntFromUint64(1234),
+					),
+					Address: constants.Alice_Num0.Owner,
+				},
+			},
+		},
+		"/dydxprotocol.bridge.MsgAcknowledgeBridgesResponse": nil,
 		// clob
 		"/klyraprotocol.clob.MsgProposedOperations": &clobtypes.MsgProposedOperations{
 			OperationsQueue: make([]clobtypes.OperationRaw, 0),

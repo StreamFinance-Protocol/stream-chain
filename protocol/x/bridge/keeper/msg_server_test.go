308e31d2d98431aa017f653167f1c39d7a63c1a2
0xDro <alegala21@gmail.com>
2025-01-08 15:12:42 -0800

    [BMOD-56] Testing: add bridge keeper tests

---
+++ protocol/x/bridge/keeper/msg_server_test.go
@@ -0,0 +1,18 @@
+package keeper_test
+
+import (
+	"context"
+	"testing"
+
+	testapp "github.com/StreamFinance-Protocol/stream-chain/protocol/testutil/app"
+	"github.com/StreamFinance-Protocol/stream-chain/protocol/x/bridge/keeper"
+	"github.com/StreamFinance-Protocol/stream-chain/protocol/x/bridge/types"
+)
+
+func setupMsgServer(t *testing.T) (keeper.Keeper, types.MsgServer, context.Context) {
+	tApp := testapp.NewTestAppBuilder(t).Build()
+	ctx := tApp.InitChain()
+	k := tApp.App.BridgeKeeper
+
+	return k, keeper.NewMsgServerImpl(k), ctx
+}

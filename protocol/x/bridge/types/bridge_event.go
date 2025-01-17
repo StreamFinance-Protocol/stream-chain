5b2b25aeb30dde8bd4a8cb3c7a2dc3200ce039b2
0xDro <alegala21@gmail.com>
2025-01-17 17:24:59 -0400

    [BW-5] Modify eqaulity check on bridge events to account for withdraws

--- protocol/x/bridge/types/bridge_event.go
+++ protocol/x/bridge/types/bridge_event.go
@@ -2,5 +2,6 @@ package types
 
 func (b BridgeEvent) Equal(other BridgeEvent) bool {
 	return b.Id == other.Id && b.Coin.Equal(other.Coin) &&
-		b.Address == other.Address && b.EthBlockHeight == other.EthBlockHeight
+		b.Address == other.Address && b.BlockHeight == other.BlockHeight &&
+		b.IsDeposit == other.IsDeposit
 }

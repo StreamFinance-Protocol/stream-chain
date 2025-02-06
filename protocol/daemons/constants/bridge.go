0f6d05c87ffaa64c4a84a57128debe1bc9399cf2
ea-coding <92234415+ea-coding@users.noreply.github.com>
2025-02-06 21:49:41 +0000

    [BW-59] Logic: Change bridge event abi

--- protocol/daemons/constants/bridge.go
+++ protocol/daemons/constants/bridge.go
@@ -1,47 +1,41 @@
 package constants
 
 const (
-	// Signature of the Bridge log, i.e. sha256("Bridge(uint256,uint256,address,bytes,bytes)").
-	BridgeEventSignature = "0x498a04382650bc110983392ed12ab27595af8ece270a344fc70d773d2481043a"
+    // Signature of the Bridge log, i.e. keccak256("Bridge(uint256,uint256,address,bytes)").
+    BridgeEventSignature = "0x44c030c7e1e9f91d70f7844a840850259bdd9d16a7423b8aae3cdc2cd497720e"
 
-	// ABI (application binary interface) of the Bridge Event.
-	BridgeEventABI = `[
-		{
-			"anonymous":false,
-			"inputs":[
-				{
-					"indexed":true,
-					"internalType":"uint256",
-					"name":"id",
-					"type":"uint256"
-				},
-				{
-					"indexed":false,
-					"internalType":"uint256",
-					"name":"amount",
-					"type":"uint256"
-				},
-				{
-					"indexed":false,
-					"internalType":"address",
-					"name":"from",
-					"type":"address"
-				},
-				{
-					"indexed":false,
-					"internalType":"bytes",
-					"name":"accAddress",
-					"type":"bytes"
-				},
-				{
-					"indexed":false,
-					"internalType":"bytes",
-					"name":"data",
-					"type":"bytes"
-				}
-			],
-			"name":"Bridge",
-			"type":"event"
-		}
-	]`
+    // ABI of the Bridge Event.
+    BridgeEventABI = `[
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "uint256",
+                    "name": "id",
+                    "type": "uint256"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "from",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "bytes",
+                    "name": "toAddress",
+                    "type": "bytes"
+                }
+            ],
+            "name": "Bridge",
+            "type": "event"
+        }
+    ]`
 )

8e0fb5045da25f422037a65ac6044a7d881cae05
ea-coding <92234415+ea-coding@users.noreply.github.com>
2025-03-12 07:34:38 +0000

    [ENDER] fix: fix postgres upsert handling for collateral pool

--- indexer/packages/postgres/src/models/collateral-pool-model.ts
+++ indexer/packages/postgres/src/models/collateral-pool-model.ts
@@ -25,9 +25,7 @@ export default class CollateralPoolsModel extends BaseModel {
         id: { type: 'integer' },
         maxCumulativeInsuranceFundDeltaPerBlock: {
           type: 'string',
-          default: '0',
           postgresql: { type: 'bigint' },
-          parse: (value: string) => value === '' ? '0' : value,
         },
         multiCollateralAssets: {
           type: 'string',

5bcd9e036e87faa5659c9c861da6c7196aeb30bc
soletty <solal@notebooklabs.xyz>
2025-03-14 18:54:53 -0300

    [Remove Clob] PROTO: remove idnexer clob related protos

--- proto/klyraprotocol/indexer/protocol/v1/query-types.ts
+++ proto/klyraprotocol/indexer/protocol/v1/query-types.ts
@@ -150,7 +150,7 @@ export interface OrderQueryConfig extends QueryConfig {
   [QueryableField.CLIENT_METADATA]?: string;
   [QueryableField.TRIGGER_PRICE]?: string;
   [QueryableField.ROUTER_FEE_PPM]?: string;
-  [QueryableField.router_fee_owner]?: string;
+  [QueryableField.ROUTER_FEE_OWNER]?: string;
 }
 
 export interface PerpetualMarketQueryConfig extends QueryConfig {
